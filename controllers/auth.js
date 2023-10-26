f7a2f7d9e45d885c23c9eb9335a6723891b489f5
Styles <bcs_2021035@iiitm.ac.in>
2023-10-26 18:50:52 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
-import users from "../models/userModel.js";
+import users from "../models/user.js";
+import OTPModel from "../models/otp.js";
+import { sendMail } from "../lib/helpers.js";
 import jwt from "jsonwebtoken";
-import Joi from 'joi'
+import Joi from "joi";
 import bcrypt from "bcrypt";
+import otpGenerator from "otp-generator";
 
 //LOGIN
 export const loginUser = async (req, res) => {
@@ -28,7 +31,7 @@ export const loginUser = async (req, res) => {
         { name: user.name, email, role: user.role },
         process.env.JWT_SECRET,
         {
-          expiresIn: "24h",
+          expiresIn: "72h",
         }
       );
 
@@ -42,8 +45,122 @@ export const loginUser = async (req, res) => {
         .status(422)
         .json(error.details.map((detail) => detail.message).join(", "));
     }
-    
+
     return res.status(500).send(error.message);
+  }
+};
+
+export const isRegistered = async (req, res) => {
+  try {
+    const email = req.body.email;
+
+    // isRegistered
+    const user = await users.findOne({ email }, { _id: 0, username: 1 });
+    if (user) {
+      return res.status(200).send(true);
+    } else {
+      return res.status(200).send(false);
+    }
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+//SEND OTP
+export const sendOTP = async (req, res) => {
+  try {
+    if (!req.body.email) {
+      return res.status(400).json("No email provided!");
+    }
+
+    // Verify JWT for authentication
+    const token = req.headers.authorization;
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if (decoded.username) {
+        return res.status(403).json("Already logged in");
+      }
+    }
+
+    // Email Validation
+    const emailSchema = Joi.object({
+      email: Joi.string().email().required(),
+    });
+    await emailSchema.validateAsync({ email: req.body.email });
+
+    // Generate OTP and store in MongoDB
+    const otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+
+    const expiresAt = new Date();
+    expiresAt.setSeconds(expiresAt.getSeconds() + 300); // 5 minutes expiration
+
+    const otpData = new OTPModel({
+      email: req.body.email,
+      otp,
+      expiresAt,
+    });
+
+    await otpData.save();
+
+    const mailOptions = {
+      from: process.env.GMAIL_ID,
+      to: req.body.email,
+      subject: `facOTTry - Your OTP for verification is ${otp}`,
+      html: `<p>Hello,</p>
+             <p>Your OTP for verification is: <strong>${otp}</strong></p>
+             <p>Thank you for using facOTTry!</p>`,
+    };
+
+    const result = await sendMail(mailOptions);
+    if (result.accepted) return res.json(result);
+
+    res.status(500).send("Error sending OTP");
+  } catch (error) {
+    if (error.details) {
+      return res
+        .status(422)
+        .json(error.details.map((detail) => detail.message).join(", "));
+    }
+
+    return res.status(500).json(error.message);
+  }
+};
+
+//VERIFY OTP
+export const verifyOTP = async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if (decoded.username) {
+        return res.status(403).json("Already logged in");
+      }
+    }
+
+    const userEnteredOTP = req.body.otp;
+    const storedOTP = await OTPModel.findOne({
+      email: req.body.email,
+    });
+
+    if (!req.session.otp || !req.session.otpExpiry || !req.session.email) {
+      return res.status(400).send("Not generated");
+    }
+
+    if (!storedOTP) {
+      return res.status(401).send("OTP expired / Not generated");
+    }
+
+    if (storedOTP.otp === userEnteredOTP) {
+      return res.send("OTP Verified");
+    } else {
+      return res.status(403).send("Wrong OTP");
+    }
+  } catch (error) {
+    return res.status(500).json(error.message);
   }
 };
 
@@ -56,13 +173,11 @@ export const registerUser = async (req, res) => {
       role: Joi.string().required().valid("admin", "user"),
       email: Joi.string().email().required(),
       password: Joi.string().pattern(
-        new RegExp(
-          "^(?=.*[A-Za-z])(?=.*[0-9])[a-zA-Z0-9@$!%*#?&]{8,}$"
-        )
+        new RegExp("^(?=.*[A-Za-z])(?=.*[0-9])[a-zA-Z0-9@$!%*#?&]{8,}$")
       ),
     });
     await registerSchema.validateAsync(req.body);
-    
+
     const { name, email, password, role } = req.body;
 
     // Hash password & save to mongoDB
